f9297ed22d160d9d5d5fa2a76828008abc84b37e
dembo <a01709522@tec.mx>
2023-11-05 19:16:45 -0600

    add first game scene and objects

---
+++ Laboratorios/Laboratorio 1/Laboratorio 1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        //Mover vehiculo hacia adelante
+        //transform.Translate(0,0,1);
+        transform.Translate(Vector3.forward);
+    }
+}
